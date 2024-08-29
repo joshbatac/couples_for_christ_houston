8a9d7ff8c5edeae5a340743f5227a759d1fe1a7f
Joshua Batac <joshuabatac@Clarissas-Air.hsd1.tx.comcast.net>
2024-08-29 16:02:41 -0500

    backend testing from CN

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello from backend!');
+  res.send('Hello from backend!!!');
 });
 
 app.post('/submit', async (req, res) => {
